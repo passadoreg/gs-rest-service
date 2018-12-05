8a58d15493327a920da1207ac0efab9f9e2b11ce
passadoreg <gabrielpassadore@gmail.com>
2018-12-05 13:51:13 -0300

    Update Greeting.java

--- complete/src/main/java/hello/Greeting.java
+++ complete/src/main/java/hello/Greeting.java
@@ -4,10 +4,14 @@ public class Greeting {
 
     private final long id;
     private final String content;
+    private final String ip;
+    private final String mac;
 
-    public Greeting(long id, String content) {
+    public Greeting(long id, String content, String ip, String mac) {
         this.id = id;
         this.content = content;
+        this.ip = ip;
+        this.mac = mac;
     }
 
     public long getId() {
@@ -16,5 +20,13 @@ public class Greeting {
 
     public String getContent() {
         return content;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public String getMac() {
+        return mac;
     }
 }
