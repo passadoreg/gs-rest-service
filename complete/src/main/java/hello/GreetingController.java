47efd2e2a7e1e66435b00fab8ac860340ffabb36
passadoreg <gabrielpassadore@gmail.com>
2018-12-05 13:51:53 -0300

    Update GreetingController.java

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -1,5 +1,9 @@
 package hello;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +17,41 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+
+        InetAddress ip;
+        String ipAddress = "", macAddress = "";
+        try {
+
+            ip = InetAddress.getLocalHost();
+            ipAddress = ip.getHostAddress();
+            System.out.println("Current IP address : " + ip.getHostAddress());
+
+            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+
+            byte[] mac = network.getHardwareAddress();
+
+            if (mac != null) {
+                System.out.print("Current MAC address : ");
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < mac.length; i++) {
+                    sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+                }
+                macAddress = sb.toString();
+                System.out.println(sb.toString());
+            }
+
+        } catch (UnknownHostException e) {
+
+            e.printStackTrace();
+
+        } catch (SocketException e){
+
+            e.printStackTrace();
+
+        }
+
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+                            String.format(template, name), ipAddress, macAddress);
     }
 }
